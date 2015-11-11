2486ef361fb7ac5089d1dc01c2ebf179c6b9a525
georgelviv <lvivgeorge@gmail.com>
2015-11-11 17:43:35 +0200

    Test for error test pass

--- app/front/app/app.spec.js
+++ app/front/app/app.spec.js
@@ -1,5 +1,7 @@
-describe('A suite', function() {
-  it('contains spec with an expectation', function() {
-    expect(true).toBe(true);
+describe('App ', function() {
+  beforeEach(module('app'));
+
+  it('should depend on existing modules', function() {
+    expect(true).toBe(false);
   });
 });
