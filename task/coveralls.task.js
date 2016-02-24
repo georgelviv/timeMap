e63006e74121c1caa488d88e25a539ef8cce3c1e
georgelviv <lvivgeorge@gmail.com>
2016-02-24 11:50:27 +0200

    Fixed path lcov.info

--- task/coveralls.task.js
+++ task/coveralls.task.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var coveralls = require('gulp-coveralls');
 var path = require('../config').path;
 
-var srcFile = [path.testDir, path.coverageLcovSuffixDir, 'lcov.info'].join('/');
+var srcFile = [path.coverageDir, path.coverageLcovSuffixDir, 'lcov.info'].join('/');
 
 module.exports = coverallsTask;
 
